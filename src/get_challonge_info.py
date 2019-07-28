2a0ae86770389d80bac4be4a96b458e650059aee
Matthew Emery <atriumcomplex@gmail.com>
2019-07-28 15:36:27 -0700

    get_challonge_info script works

---
+++ src/get_challonge_info.py
@@ -0,0 +1,26 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+from pathlib import Path
+import json
+
+def get_api_key():
+    load_dotenv()
+    return os.getenv("CHALLONGE_API")
+
+
+def get_challonge_api_info(tournament, api_key):
+    return requests.get(f"https://api.challonge.com/v1/tournaments/{tournament}.json",
+                 params={'api_key': api_key,
+                         'include_participants': 1,
+                         'include_matches': 1
+                         })
+
+
+if __name__ == '__main__':
+    api_key = get_api_key()
+    results = get_challonge_api_info('madness19', api_key)
+
+    with open(Path("../data/madness19-post.json"), 'w+') as file_path:
+        json.dump(results.json(), file_path, indent=2)
